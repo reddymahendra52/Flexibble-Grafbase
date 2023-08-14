67943401b8129b90e7c467323316c7251a253e97
Mutukundu Mahendra Reddy <reddymahendra52@gmail.com>
2023-08-14 14:35:13 +0530

    added developer link

--- constants/index.ts
+++ constants/index.ts
@@ -1,9 +1,16 @@
 export const NavLinks = [
   { href: "/", key: "Inspiration", text: "Inspiration" },
   { href: "/", key: "Find Projects", text: "Find Projects" },
-  { href: "/", key: "Learn Development", text: "Learn Development" },
-  { href: "/", key: "Career Advancement", text: "Career Advancement" },
-  { href: "/", key: "Hire Developers", text: "Hire Developers" },
+  {
+    href: "/https://www.freecodecamp.org/",
+    key: "Learn Development",
+    text: "Learn Development",
+  },
+  {
+    href: "https://reddymahendra52.mmm.page/main",
+    key: "Hire Me",
+    text: "Hire Me",
+  },
 ];
 
 export const categoryFilters = [
@@ -14,12 +21,8 @@ export const categoryFilters = [
   "UI/UX",
   "Game Dev",
   "DevOps",
-  "Data Science",
   "Machine Learning",
   "Cybersecurity",
-  "Blockchain",
-  "E-commerce",
-  "Chatbots",
 ];
 
 export const footerLinks = [
